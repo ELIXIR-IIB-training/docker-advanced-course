a62503a1c42f064422fa2f948a01ce7cd6a4157e
Gianluca Della Vedova <gianluca@dellavedova.org>
2018-09-27 14:56:01 +0200

    Final exercise

--- solutions/2-4-01-comparing-aligners/entrypoint.sh
+++ solutions/2-4-01-comparing-aligners/entrypoint.sh
@@ -9,7 +9,14 @@ groupadd -g "$GROUP_ID" group
 useradd --shell /bin/bash -u "$USER_ID" -g group -o -c "" -m user
 export HOME=/
 chown --recursive "$USER_ID":"$GROUP_ID" /data
+echo "$PATH"
 
-exec gosu user /opt/bin/cutadapt -a AACCGGTT -o /data/.temp.fastq $1
-exec gosu user /code/fastq-tools/fastq-uniq /data/.temp.fastq $2
-exec gosu user rm -f /data/.temp.fastq
+cd /data
+echo "bwa index"
+gosu user bwa index lambda_virus.fa.gz
+echo "bwa aln"
+gosu user bwa aln lambda_virus.fa.gz reads_1.fq.gz -f reads_1.fq.gz.sai
+echo "bwa samse"
+gosu user bwa samse lambda_virus.fa.gz reads_1.fq.gz.sai reads_1.fq.gz -f results
+echo "bowtie2"
+gosu user bowtie2 -x lambda_virus -U reads_1.fq.gz
